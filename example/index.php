92d5e996b7b745ab2c03fddc6fd4f172999559f6
Will Blew <wblew@linode.com>
2017-03-21 15:54:15 +0000

    Semicolons and working code

--- example/index.php
+++ example/index.php
@@ -10,8 +10,8 @@ if (!empty($config['token'])) {
     echo '$Instances = new Linode\Instances\Instances();<BR>';
     echo '$Instances->getLinodes();';
     echo '<BR><BR><h3>Create a Linode</h3>';
-    echo '$create = new Linode\Instances\Instances();'
-    echo '$create->createLinode('newark','standard-1','coolName','coolGroup',null,'CoolPassword!',null,null,null,null,'true');'
+    echo '$create = new Linode\Instances\Instances();<BR>';
+    echo '$create->createLinode("newark","standard-1","coolName","coolGroup",null,"CoolPassword!",null,null,null,null,"true");';
 } else {
     echo "You didn't provide a token in your config! Provide it or pass a code to set one.";
 }
