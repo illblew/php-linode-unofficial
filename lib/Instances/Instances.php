d3d4a9dc56930fc269cdcec0fd30a7ac9ccaf0d7
Marques Johansson <displague@gmail.com>
2017-06-22 03:24:57 +0000

    updated the example and related endpoints for API changes
    Can now be invoked at the command line:
    ```
    examples$ php -d include_path=`pwd`/example index.php
    ```

--- lib/Instances/Instances.php
+++ lib/Instances/Instances.php
@@ -25,12 +25,23 @@ class Instances extends Core
         $apiUrl = $this->getApiUrl();
         $token = $this->getTokenAuth();
         $fullUrl = $apiUrl . 'linode/instances';
-        $arr = array("datacenter" => $datacenter, "type" => $type, "label" => $label, "group" => $group, "distro" => $distro, "root_pass" => $root_pass, "root_ssh_key" => $root_ssh_key, "stackscript" => $stackscript, "stackscript_udf_responses" => $stackscript_udf_responses, "backup" => $backup, "with_backups" => $with_backup);
+        $arr = array("region" => $datacenter, "type" => $type, "label" => $label, "group" => $group, "distro" => $distro, "root_pass" => $root_pass, "root_ssh_key" => $root_ssh_key, "stackscript" => $stackscript, "stackscript_udf_responses" => $stackscript_udf_responses, "backup" => $backup, "with_backups" => $with_backup);
         $post = json_encode($arr);
         $header = array ('Content-Type: application/json', 'Authorization: token ' . $token);
         $instance = $curl->curlPost($fullUrl,$header,$post);
         return $instance;
 
+    }
+
+    //Delete a Linode Instance.
+    public function deleteLinode($id) {
+        $curl = new Curl();
+        $apiUrl = $this->getApiUrl();
+        $token = $this->getTokenAuth();
+        $fullUrl = $apiUrl . "linode/instances/".((int)$id);
+        $header = array ('Content-Type: application/json', 'Authorization: token ' . $token);
+        $instance = $curl->curlDelete($fullUrl,$header);
+        return $instance;
     }
 
 }
