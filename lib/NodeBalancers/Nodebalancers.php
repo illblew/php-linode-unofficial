a894dc2ed983f30de5645cbc8d42e904289eeafa
Will Blew <will@willblew.com>
2018-03-20 21:02:11 +0000

    Introduces nodebal and nodebal configs

--- lib/NodeBalancers/Nodebalancers.php
+++ lib/NodeBalancers/Nodebalancers.php
@@ -15,6 +15,49 @@ class NodeBalancers extends Core
         $nodebalancers = $curl->curlGet($fullUrl,$header);
         return $nodebalancers;
     }
+
+    public function putNodeBalancers($region = null,$label = null,$client_conn_throttle = 0) {
+        $curl = new Curl();
+        $apiUrl = $this->getApiUrl();
+        $header = $this->getHeader(True);
+        $fullUrl = $apiUrl . 'nodebalancers/';
+        $arr = array('region' => $region, 'label' => $label, 'client_conn_throttle' => $client_conn_throttle);
+        $post = json_encode($arr);
+        $nodebalancer = $curl->curlPost($fullUrl,$header,$post);
+    }
+
+    public function deleteNodeBalancer($nb_id) {
+        $curl = new Curl();
+        $apiUrl =  $this->getApiUrl();
+        $header = $this->getHeader(True);
+        $fullUrl = $apiUrl . 'nodebalancers/' . $nb_id;
+        $nodebalancer = $curl->curlDelete($fullUrl,$header);
+        return $nodebalancer;
+    }
+
+    // Configs
+    
+    public function getNodeConfig($nb_id) {
+        $curl = new Curl();
+        $apiUrl = $this->getApiUrl();
+        $header = $this->getHeader(True);
+        $fullUrl = $apiUrl . 'nodebalancers/' . $nb_id . '/configs';
+        $nodeConfig = $curl->curlGet($fullUrl,$header);
+        return $nodeConfig;
+    }
+
+    public function postNodeConfig($nb_id,$port = null, $protocol = null, $algorithm = null, $stickiness = null, $check = null, $check_interval = null, $check_timeout = null, $check_attempts = null, $check_path = null, $check_body = null, $check_passive = null, $ssl_cert = null, $ssl_key = null, $cipher_suite = null) {
+        $curl = new Curl();
+        $apiUrl = $this->getApiUrl();
+        $header = $this->getHeader(True);
+        $fullUrl = $apiUrl . 'nodebalacer/' . $nb_id . '/configs';
+        $arr = array('port' => $port, 'protocol' => $protocol, 'algorithm' => $algorithm, 'stickiness' => $stickiness, 'check' => $check, 'check_interval' => $check_interval, 'check_timeout' => $check_interval, 'check_attempts' => $check_interval, 'check_path' => $check_path, 'check_body' => $check_body, 'check_passive' => $check_passive, 'ssl_cert' => $ssl_cert, 'ssl_key' => $ssl_key, 'cipher_suite' => $cipher_suite);
+
+        $post = json_encode($arr);
+        $config = $curl->curlPost($fullUrl,$header,$post);
+        return $config;
+    }
+
 }
 
 ?>
